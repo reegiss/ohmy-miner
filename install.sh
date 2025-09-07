4c0d412676efcc6490c809bbe4b02eef136f807b
Regis Araujo <regis@r3tecnologia.net>
2025-09-07 14:51:27 -0300

    feat: fix build

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d build ]; then
+    mkdir build
+fi
+
 # Linux build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug .. && make && ./ohmy-miner --algo qhash --url qubitcoin.luckypool.io:8610 --user bc1qacadts4usj2tjljwdemfu44a2tq47hch33fc6f.R3G --pass x
